60af2ed1016b656615047ec619e5bbebff6de396
bayazzit <bayazitb95@gmail.com>
2024-05-20 00:01:15 +0300

    locators are updated

--- locators/locators.py
+++ locators/locators.py
@@ -4,13 +4,13 @@ password_box = "id:password"
 login_button = "id:login-button"
 
 # All Products Page
-go_to_cart = ".shopping_cart_link"
-backpack_item = "a[id='item_4_title_link'] div[class='inventory_item_name ']"
+go_to_cart = "class:shopping_cart_link"
+backpack_item = "css:a[id='item_4_title_link'] div[class='inventory_item_name ']"
 
 # Specific Product Page
 add_to_cart_button = "id:add-to-cart"
-product_title = ".inventory_details_name.large_size"
+product_title = "class:inventory_details_name.large_size"
 
 # Cart Page
-remove_button = "(//button[normalize-space()='Remove'])[1]"
-item_name = ".inventory_item_name"
+remove_button = "xpath:(//button[normalize-space()='Remove'])[1]"
+item_name = "class:inventory_item_name"
